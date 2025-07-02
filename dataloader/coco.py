50b17627b22ab0370b6f858bc7db5f2691835dd9
tuyen.dn <dongoctuyenn@gmail.com>
2025-07-02 14:28:04 +0700

    update

--- dataloader/coco.py
+++ dataloader/coco.py
@@ -204,8 +204,7 @@ class ConvertCocoPolysToMask(object):
 
 def make_coco_transforms(image_set, cautious):
 
-    normalize = T.Compose([T.ToTensor()])
-    # , T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    normalize = T.Compose([T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
